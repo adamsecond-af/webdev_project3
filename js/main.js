c06a5a9f1838054117c9dd58dfae28aa7787313d
AdamLaptop <adamsecond94@gmail.com>
2026-04-15 22:18:12 -0500

    feat: add markers and two places to map

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 function initMap() {
   const mapEl = document.getElementById('map');
   if (!mapEl) {
@@ -5,16 +6,41 @@ function initMap() {
   }
 
   const chicago = { lat: 41.8832, lng: -87.6324 };
-
   const map = new google.maps.Map(mapEl, {
     center: chicago,
     zoom: 11,
   });
 
-  const infoWindow = new google.maps.InfoWindow({
-    content: 'Welcome to Chicago!',
-    position: chicago
-  });
+  const places = [
+    {
+      position: { lat: 41.8826, lng: -87.6226 },
+      title: 'Millennium Park',
+      info: 'Home of the Bean and one of my favorite spots in the city.'
+    },
+    {
+      position: { lat: 41.8789, lng: -87.6359 },
+      title: 'Willis Tower',
+      info: 'The tallest building in Chicago with an incredible Skydeck.'
+    },
+  ];
 
-  infoWindow.open(map);
+  const infoWindow = new google.maps.InfoWindow();
+  places.forEach(function (place) {
+    const marker = new google.maps.Marker({
+      position: place.position,
+      map: map,
+      title: place.title
+    });
+
+    marker.addListener('click', function () {
+      infoWindow.setContent(
+        '<div style="color:#0d0d0d"><strong>' +
+          place.title +
+          '</strong><br>' +
+          place.info +
+          '</div>'
+      );
+      infoWindow.open(map, marker);
+    });
+  });
 }
